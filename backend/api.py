0cf8ba37089a17a04c5deb6b46bcb5214b997c45
Yiting Qiang <qiangyt@wxcount.com>
2024-12-17 14:23:37 +0800

    replace console with fastapi

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,16 @@
+from pprint import pprint
 from fastapi import FastAPI
 from pydantic import BaseModel
-import uvicorn
-import os
+
 from dotenv import load_dotenv
+from workflow import workflow
 
 load_dotenv()
 
-app = FastAPI()
+# Compile
+compiled_workflow = workflow.compile()
+
+fastapi_app = FastAPI()
 
 class QuestionRequest(BaseModel):
     question: str
@@ -15,11 +19,20 @@ class AnswerResponse(BaseModel):
     question: str
     answer: str
 
-@app.post("/rest/question", response_model=AnswerResponse)
+@fastapi_app.post("/rest/question", response_model=AnswerResponse)
 async def get_answer(request: QuestionRequest):
     question = request.question.strip()
-    answer = "Sorry, I don't know the answer to that question."
-    return {"question": question, "answer": answer}
+    inputs = {
+        "question": question
+    }
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=4080)
+    for output in compiled_workflow.stream(inputs):
+        for key, value in output.items():
+            # Node
+            pprint(f"Node '{key}':")
+            # Optional: print full state at each node
+            # pprint.pprint(value["keys"], indent=2, width=80, depth=None)
+        pprint("\n---\n")
+    
+    answer = value["generation"]#"Sorry, I don't know the answer to that question."
+    return {"question": question, "answer": answer}
--- backend/main.py
+++ backend/main.py
@@ -1,36 +1,6 @@
-import getpass
-import os
+import uvicorn
 
-from pprint import pprint
+from api import fastapi_app
 
-### from langchain_cohere import CohereEmbeddings
-
-from workflow import workflow
-
-def _set_env(var: str):
-    if not os.environ.get(var):
-        os.environ[var] = getpass.getpass(f"{var}: ")
-
-
-_set_env("OPENAI_API_KEY")
-
-
-# Compile
-app = workflow.compile()
-
-### Use Graph ########################################################################################################
-
-# Run
-inputs = {
-    "question": "What player at the Bears expected to draft first in the 2024 NFL draft?"
-}
-for output in app.stream(inputs):
-    for key, value in output.items():
-        # Node
-        pprint(f"Node '{key}':")
-        # Optional: print full state at each node
-        # pprint.pprint(value["keys"], indent=2, width=80, depth=None)
-    pprint("\n---\n")
-
-# Final generation
-pprint(value["generation"])
+if __name__ == "__main__":
+    uvicorn.run(fastapi_app, host="0.0.0.0", port=3001)
