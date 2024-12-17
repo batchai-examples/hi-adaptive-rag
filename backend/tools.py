70ba634dc0cfd2ad48f4ed0358e6f4d289e58c9a
Yiting Qiang <qiangyt@wxcount.com>
2024-12-17 12:10:47 +0800

    refactor: move question_router, retriever, rag_chain, hallucination_grader, answer_grader, question_rewriter, web_search_tool, retrieval_grader to tools.py

---
+++ backend/tools.py
@@ -0,0 +1,252 @@
+from typing import Literal
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+from pydantic import BaseModel, Field
+### from langchain_cohere import CohereEmbeddings
+
+
+
+# create index ########################################################################################################
+### Build Index
+from langchain_core.vectorstores import VectorStoreRetriever;
+
+def build_index() -> VectorStoreRetriever:
+
+    # Set embeddings
+    embd = OpenAIEmbeddings()
+
+    # Docs to index
+    urls = [
+        "https://lilianweng.github.io/posts/2023-06-23-agent/",
+        "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+        "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+    ]
+
+    # Load
+    docs = [WebBaseLoader(url).load() for url in urls]
+    docs_list = [item for sublist in docs for item in sublist]
+
+    # Split
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=500, chunk_overlap=0
+    )
+    doc_splits = text_splitter.split_documents(docs_list)
+
+    # Add to vectorstore
+    vectorstore = Chroma.from_documents(
+        documents=doc_splits,
+        collection_name="rag-chroma",
+        embedding=embd,
+    )
+    return vectorstore.as_retriever()
+
+retriever = build_index()
+
+
+# LLMs ###################################################################################################################
+
+def build_question_router():
+    # Data model
+    class RouteQuery(BaseModel):
+        """Route a user query to the most relevant datasource."""
+
+        datasource: Literal["vectorstore", "web_search"] = Field(
+            ...,
+            description="Given a user question choose to route it to web search or a vectorstore.",
+        )
+
+
+    # LLM with function call
+    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+    structured_llm_router = llm.with_structured_output(RouteQuery)
+
+    # Prompt
+    system = """You are an expert at routing a user question to a vectorstore or web search.
+    The vectorstore contains documents related to agents, prompt engineering, and adversarial attacks.
+    Use the vectorstore for questions on these topics. Otherwise, use web-search."""
+    route_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            ("human", "{question}"),
+        ]
+    )
+
+    question_router = route_prompt | structured_llm_router
+    print(
+        question_router.invoke(
+            {"question": "Who will the Bears draft first in the NFL draft?"}
+        )
+    )
+    print(question_router.invoke({"question": "What are the types of agent memory?"}))
+    return question_router
+
+question_router = build_question_router()
+
+### Retrieval Grader ##################################################################################################
+def build_retrieval_grader():
+
+    # Data model
+    class GradeDocuments(BaseModel):
+        """Binary score for relevance check on retrieved documents."""
+
+        binary_score: str = Field(
+            description="Documents are relevant to the question, 'yes' or 'no'"
+        )
+
+
+    # LLM with function call
+    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+    structured_llm_grader = llm.with_structured_output(GradeDocuments)
+
+    # Prompt
+    system = """You are a grader assessing relevance of a retrieved document to a user question. \n 
+        If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
+        It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+        Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
+    grade_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            ("human", "Retrieved document: \n\n {document} \n\n User question: {question}"),
+        ]
+    )
+
+    retrieval_grader = grade_prompt | structured_llm_grader
+    
+    return retrieval_grader
+
+retrieval_grader = build_retrieval_grader()
+
+question = "agent memory"
+docs = retriever.invoke(question)
+doc_txt = docs[1].page_content
+print(retrieval_grader.invoke({"question": question, "document": doc_txt}))
+    
+
+### Generate ##########################################################################################################
+def build_rag_chain():
+    # Prompt
+    prompt = hub.pull("rlm/rag-prompt")
+
+    # LLM
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+
+    # Post-processing
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+
+    # Chain
+    rag_chain = prompt | llm | StrOutputParser()
+    return rag_chain
+
+rag_chain = build_rag_chain()
+# Run
+generation = rag_chain.invoke({"context": docs, "question": question})
+print(generation)
+
+### Hallucination Grader ################################################################################################
+
+def build_hallucination_grader():
+    # Data model
+    class GradeHallucinations(BaseModel):
+        """Binary score for hallucination present in generation answer."""
+
+        binary_score: str = Field(
+            description="Answer is grounded in the facts, 'yes' or 'no'"
+        )
+
+
+    # LLM with function call
+    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+    structured_llm_grader = llm.with_structured_output(GradeHallucinations)
+
+    # Prompt
+    system = """You are a grader assessing whether an LLM generation is grounded in / supported by a set of retrieved facts. \n 
+        Give a binary score 'yes' or 'no'. 'Yes' means that the answer is grounded in / supported by the set of facts."""
+    hallucination_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            ("human", "Set of facts: \n\n {documents} \n\n LLM generation: {generation}"),
+        ]
+    )
+
+    hallucination_grader = hallucination_prompt | structured_llm_grader
+    return hallucination_grader
+
+hallucination_grader = build_hallucination_grader()
+hallucination = hallucination_grader.invoke({"documents": docs, "generation": generation})
+#print(hallucination)
+## GradeHallucinations(binary_score='yes')
+
+### Answer Grader ######################################################################################################
+def build_answer_grader():
+
+    # Data model
+    class GradeAnswer(BaseModel):
+        """Binary score to assess answer addresses question."""
+
+        binary_score: str = Field(
+            description="Answer addresses the question, 'yes' or 'no'"
+        )
+
+
+    # LLM with function call
+    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+    structured_llm_grader = llm.with_structured_output(GradeAnswer)
+
+    # Prompt
+    system = """You are a grader assessing whether an answer addresses / resolves a question \n 
+        Give a binary score 'yes' or 'no'. Yes' means that the answer resolves the question."""
+    answer_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            ("human", "User question: \n\n {question} \n\n LLM generation: {generation}"),
+        ]
+    )
+
+    answer_grader = answer_prompt | structured_llm_grader
+    return answer_grader
+
+answer_grader = build_answer_grader()
+answer_grader.invoke({"question": question, "generation": generation})
+
+## GradeAnswer(binary_score='yes')
+
+### Question Re-writer ################################################################################################
+def build_question_rewriter():
+    # LLM
+    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+
+    # Prompt
+    system = """You a question re-writer that converts an input question to a better version that is optimized \n 
+        for vectorstore retrieval. Look at the input and try to reason about the underlying semantic intent / meaning."""
+    re_write_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            (
+                "human",
+                "Here is the initial question: \n\n {question} \n Formulate an improved question.",
+            ),
+        ]
+    )
+
+    question_rewriter = re_write_prompt | llm | StrOutputParser()
+    return question_rewriter
+
+question_rewriter = build_question_rewriter()
+question_rewriter.invoke({"question": question})
+
+## "What is the role of memory in an agent's functioning?"
+
+### Search #############################################################################################################
+
+web_search_tool = TavilySearchResults(k=3)
